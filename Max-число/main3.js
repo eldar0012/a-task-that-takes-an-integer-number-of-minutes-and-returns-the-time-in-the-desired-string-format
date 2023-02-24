3aed3922f43781da052b340ac25e6c51f4804802
eldar0012 <114676182+eldar0012@users.noreply.github.com>
2023-02-24 17:22:46 +0300

    sdsdgds

---
+++ Max-число/main3.js
@@ -0,0 +1,14 @@
+let result;
+for (let i=1;i<=4;i++){
+let numberQ=+prompt('Введите ваше число №: '+i);
+}
+function gds(numberQ){
+	if (numberQ==Number)
+result=+Math.max(numberQ);
+alert(result);
+}
+else
+{
+	alert('Это не число');
+}
+gds(result);
---
+++ звезда/main2.js
@@ -0,0 +1,11 @@
+let qwer=" ";
+let zvezda= "*";
+function frt(hai) {
+	
+	for (let i=0;i<hai;i++){
+		
+		qwer+=zvezda;
+		console.log(qwer)
+	}
+} 
+frt(8);
